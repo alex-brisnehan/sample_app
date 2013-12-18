a9cf1b27c7ad4d7bae1cd8599e4b9824f259b04f
saasbook <saasbook@saasbook.(none)>
2013-12-17 22:43:53 -0800

    Change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,6 @@ SampleApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.assets.precompile += %w( custom.css  )
   config.cache_classes = true
   config.serve_static_assets = true
   config.assets.compile = true
